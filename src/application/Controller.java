4d95741ac5f85aa72be1a6f604052890f742c800
kasperdinsen <kasperdinsen@gmail.com>
2020-01-13 10:17:01 +0100

    Major overhall1

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -8,6 +8,7 @@ import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -26,6 +27,10 @@ import java.util.ResourceBundle;
 public class Controller implements Initializable {
 
     public static ArrayList<String> videoPath = new ArrayList<>();
+
+    @FXML
+    ScrollPane parentScrollPane;
+
 
     @FXML
     Button video1;
@@ -62,6 +67,9 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        checkAddedVideosPlaylist();
+
+
         /*video1.setOnAction(event -> {
             try {
                 startVideo((Stage)video1.getScene().getWindow());
@@ -80,6 +88,8 @@ public class Controller implements Initializable {
     @FXML private void showYourPlaylist() {
         ArrayList<String> imagePath = new ArrayList<>();
         ArrayList<String> videoInfo = new ArrayList<>();
+        videoPath.clear();
+        playlistGrid.getChildren().clear();
 
 
         thumbnailHolder.setVisible(false);
@@ -90,7 +100,9 @@ public class Controller implements Initializable {
         playlistGrid.setVisible(true);
         searchGrid.setVisible(false);
 
-        DB.selectSQL("Select fldThumbnailPath from tblVideo;");
+        DB.selectSQL("select fldThumbnailPath from tblVideo\n" +
+                "join tblVideoPlaylist on tblVideo.fldVideoID = tblVideoPlaylist.fldVideoID\n" +
+                "join tblPlaylist on tblVideoPlaylist.fldPlaylistID = tblPlaylist.fldPlaylistID;");
         do{
             String data = DB.getDisplayData();
             if (data.equals(DB.NOMOREDATA)){
@@ -146,7 +158,7 @@ public class Controller implements Initializable {
             playlistGrid.setVgap(40);
             thumbnail.setOnMouseClicked(event -> {
                 try {
-                    startVideo((Stage)thumbnail.getScene().getWindow(), videoP);
+                    startVideo((Stage)parentScrollPane.getScene().getWindow(), videoP);
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
@@ -155,7 +167,7 @@ public class Controller implements Initializable {
         }
         int rowIndex = 0;
 
-        for (int j = 0; j < 3; j++) {
+        for (int j = 0; j < (videoInfo.size()/4); j++) {
             TextArea info = new TextArea();
             info.setStyle("-fx-font-size: 16;");
             info.setMaxHeight(250.0);
@@ -187,39 +199,116 @@ public class Controller implements Initializable {
     }
 
     private void startVideo(Stage stage1, String path) throws Exception {
+        System.out.println(path);
         FXMLLoader loader = new FXMLLoader(getClass().getResource("mediaplayer.fxml"));
         MediaPlayerController mediaPlay = new MediaPlayerController(path);
         loader.setController(mediaPlay);
         Parent root = loader.load();
         stage1.setScene(new Scene(root, 1000,1000));
+        //stage1.setFullScreen(true);
         //Parent root = FXMLLoader.load(getClass().getResource("mediaplayer.fxml"));
         //stage1.setTitle("Hello World");
         //stage1.setScene(new Scene(root, 1000, 1000));
 
     }
 
+    @FXML public void checkAddedVideosPlaylist() {
+        DB.selectSQL(("SELECT fldVideoID FROM tblVideoPlaylist WHERE fldVideoID = 3"));
+        if(!DB.getData().equals("|ND|")) {
+            DB.getData().trim();
+            buttonVideo1.setText("Remove from Playlist");
+            buttonVideo1.setStyle("-fx-background-color: #ffbf00;");
+            buttonVideo1.setOnAction(event -> deleteVideo1FromPlaylist());
+        }
+
+        DB.selectSQL(("SELECT fldVideoID FROM tblVideoPlaylist WHERE fldVideoID = 2"));
+        if(!DB.getData().equals("|ND|")) {
+            DB.getData().trim();
+            buttonVideo2.setText("Remove from Playlist");
+            buttonVideo2.setStyle("-fx-background-color: #ffbf00;");
+            buttonVideo2.setOnAction(event -> deleteVideo2FromPlaylist());
+        }
+
+
+        DB.selectSQL(("SELECT fldVideoID FROM tblVideoPlaylist WHERE fldVideoID = 5"));
+        if(!DB.getData().equals("|ND|")) {
+            DB.getData().trim();
+            buttonVideo3.setText("Remove from Playlist");
+            buttonVideo3.setStyle("-fx-background-color: #ffbf00;");
+            buttonVideo3.setOnAction(event -> deleteVideo3FromPlaylist());
+        }
+
+        DB.selectSQL(("SELECT fldVideoID FROM tblVideoPlaylist WHERE fldVideoID = 4"));
+        if(!DB.getData().equals("|ND|")) {
+            DB.getData().trim();
+            buttonVideo4.setText("Remove from Playlist");
+            buttonVideo4.setStyle("-fx-background-color: #ffbf00;");
+            buttonVideo4.setOnAction(event -> deleteVideo4FromPlaylist());
+        }
+
+
+    }
+
     @FXML public void addVideo1ToPlaylist() {
-        //B.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES("+ 1 +","+ 1 +")");
-        DB.selectSQL("Select fldThumbnailPath from tblVideo where fldVideoID ='3';");
-        String path = DB.getData();
-        playlist1Thumbnail.setImage(new Image(path));
+        DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES('3','1')");
+        buttonVideo1.setText("Remove From Playlist");
+        buttonVideo1.setStyle("-fx-background-color: #ffbf00;");
+        buttonVideo1.setOnAction(event -> deleteVideo1FromPlaylist());
 
+    }
 
+    @FXML public void deleteVideo1FromPlaylist() {
+        DB.deleteSQL("DELETE FROM tblVideoPlaylist Where fldVideoID = 3");
+        buttonVideo1.setText("Add To Playlist");
+        buttonVideo1.setStyle("-fx-background-color: #B20000;");
+        buttonVideo1.setOnAction(event -> addVideo1ToPlaylist());
 
     }
 
     @FXML public void addVideo2ToPlaylist() {
-        DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES("+ 2 +","+ 1 +")");
+        DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES('2' ,'1')");
+        buttonVideo2.setText("Remove From Playlist");
+        buttonVideo2.setStyle("-fx-background-color: #ffbf00;");
+        buttonVideo2.setOnAction(event -> deleteVideo2FromPlaylist());
+
+    }
+
+    @FXML public void deleteVideo2FromPlaylist() {
+        DB.deleteSQL("DELETE FROM tblVideoPlaylist Where (fldVideoID) = '2'");
+        buttonVideo2.setText("Add To Playlist");
+        buttonVideo2.setStyle("-fx-background-color: #B20000;");
+        buttonVideo2.setOnAction(event -> addVideo2ToPlaylist());
 
     }
 
     @FXML public void addVideo3ToPlaylist() {
-        DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES("+ 3 +","+ 1 +")");
+        DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES("+ 5 +","+ 1 +")");
+        buttonVideo3.setText("Remove From Playlist");
+        buttonVideo3.setStyle("-fx-background-color: #ffbf00;");
+        buttonVideo3.setOnAction(event -> deleteVideo3FromPlaylist());
+    }
+
+    @FXML public void deleteVideo3FromPlaylist() {
+        DB.deleteSQL("DELETE FROM tblVideoPlaylist Where (fldVideoID) = '5'");
+        buttonVideo3.setText("Add To Playlist");
+        buttonVideo3.setStyle("-fx-background-color: #B20000;");
+        buttonVideo3.setOnAction(event -> addVideo3ToPlaylist());
 
     }
 
     @FXML public void addVideo4ToPlaylist() {
         DB.insertSQL("INSERT INTO tblVideoPlaylist (fldVideoID, fldPlaylistID) VALUES("+ 4 +","+ 1 +")");
+        buttonVideo4.setText("Remove From Playlist");
+        buttonVideo4.setStyle("-fx-background-color: #ffbf00;");
+        buttonVideo4.setOnAction(event -> deleteVideo4FromPlaylist());
+    }
+
+    @FXML public void deleteVideo4FromPlaylist() {
+        DB.deleteSQL("DELETE FROM tblVideoPlaylist Where (fldVideoID) = '4'");
+        buttonVideo4.setText("Add To Playlist");
+        buttonVideo4.setStyle("-fx-background-color: #B20000;");
+        buttonVideo4.setOnAction(event -> addVideo4ToPlaylist());
+
     }
 
     @FXML public void showPlaylist() {
@@ -248,8 +337,10 @@ public class Controller implements Initializable {
     }
 
     @FXML public void searchVideos() {
-        searchGrid.getChildren().clear();
         ArrayList<String> imagePath = new ArrayList<>();
+        ArrayList<String> videoPathSearch = new ArrayList<>();
+        searchGrid.getChildren().clear();
+
 
         thumbnailHolder.setVisible(false);
         playlistGrid.setVisible(false);
@@ -268,9 +359,22 @@ public class Controller implements Initializable {
             }
         } while(true);
 
+        DB.selectSQL("SELECT fldMediaPath FROM tblVideo WHERE MATCH(fldVideoName) AGAINST('"+userSearchQuery+"');");
+        do{
+            String data = DB.getDisplayData();
+            if (data.equals(DB.NOMOREDATA)){
+                break;
+            }else{
+                videoPathSearch.add(data);
+
+            }
+        } while(true);
+
         for (int i = 0; i <imagePath.size() ; i++) {
             //System.out.println(imagePath.get(i));
             String path = imagePath.get(i);
+            String videoPath1 = videoPathSearch.get(i);
+            System.out.println(videoPath1);
             ImageView thumbnail = new ImageView();
             thumbnail.setFitWidth(201);
             thumbnail.setFitHeight(280);
@@ -280,13 +384,14 @@ public class Controller implements Initializable {
             searchGrid.add(thumbnail, 0, i);
             GridPane.setHalignment(thumbnail, HPos.CENTER);
             searchGrid.setVgap(40);
-           /* thumbnail.setOnMouseClicked(event -> {
+            System.out.println(videoPath1);
+            thumbnail.setOnMouseClicked(event -> {
                 try {
-                    startVideo((Stage)video1.getScene().getWindow());
+                    startVideo((Stage)parentScrollPane.getScene().getWindow(), videoPath1);
                 } catch (Exception e) {
                     System.out.println(e.getMessage());
                 }
-            });*/
+            });
 
         }
 
--- src/application/MediaPlayerController.java
+++ src/application/MediaPlayerController.java
@@ -1,27 +1,67 @@
 package application;
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.DoubleProperty;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.HPos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.*;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
+
+import java.awt.*;
 import java.io.*;
 import java.net.*;
 import java.util.ArrayList;
+import java.util.ListIterator;
+import java.util.Observable;
 import java.util.ResourceBundle;
 
 public class MediaPlayerController implements Initializable {
+
+    ListIterator<String> it = Controller.videoPath.listIterator();
+
 
     public MediaPlayerController(String path) {
         this.mediaPath = path;
 
     }
+
+    @FXML
+    GridPane mediaPlaylistGrid;
+
+    @FXML
+    Slider sliderSeek;
+
+    @FXML
+    Label labelCurrentTime;
+
+    @FXML Label labelTotalDuration;
+
+    @FXML Slider sliderSound;
+
+    @FXML Label labelCurrentSoundVolume;
+
+    @FXML Button buttonFullscreen;
+
+    @FXML private ImageView imagePlay;
+
+    @FXML
+    AnchorPane mediaplayerMenu;
 
 
     private String mediaPath;
@@ -40,11 +80,48 @@ public class MediaPlayerController implements Initializable {
 
     @FXML private Button back;
 
+    @FXML private Button buttonNext;
+
+
     private MediaPlayer mp;
     private Media me;
 
+    @FXML public void handleShowPlaylistGrid() {
+        mediaPlaylistGrid.setVisible(true);
+    }
+
+    @FXML public void handleDisablePlaylistGrid() {
+        mediaPlaylistGrid.setVisible(false);
+    }
 
 
+
+
+    InvalidationListener sliderChangeListener = o-> {
+        Duration seekTo = Duration.seconds((int)sliderSeek.getValue());
+        System.out.println(mp.getTotalDuration().toSeconds());
+        mp.seek(seekTo);
+    };
+
+    InvalidationListener currentTimeChangeListener = o-> {
+        if((int)mp.getCurrentTime().toSeconds() == (int)mp.getTotalDuration().toSeconds()) {
+            handleStop();
+        }
+        sliderSeek.setMax(mp.getTotalDuration().toSeconds());
+        labelCurrentTime.setText(String.valueOf((int)mp.getCurrentTime().toSeconds()));
+        sliderSeek.valueProperty().removeListener(sliderChangeListener);
+        sliderSeek.setValue((int)mp.getCurrentTime().toSeconds());
+        sliderSeek.valueProperty().addListener(sliderChangeListener);
+
+
+    };
+
+    InvalidationListener sliderSoundListener = o-> {
+        mp.setVolume(sliderSound.getValue());
+        labelCurrentSoundVolume.setText(String.valueOf((int)(sliderSound.getValue()*100)));
+
+
+    };
 
     /**
      * This method is invoked automatically in the beginning. Used for initializing, loading data etc.
@@ -55,6 +132,44 @@ public class MediaPlayerController implements Initializable {
     public void initialize(URL location, ResourceBundle resources){
         // Build the path to the location of the media file
         //String path = new File("src/presentation/ressources/videofiles/joker.mp4").getAbsolutePath()";
+        //sliderSeek.setMax((int)mp.getTotalDuration().toSeconds());
+        ArrayList<String> imagePath = new ArrayList<>();
+
+        DB.selectSQL("select fldThumbnailPath from tblVideo\n" +
+                "join tblVideoPlaylist on tblVideo.fldVideoID = tblVideoPlaylist.fldVideoID\n" +
+                "join tblPlaylist on tblVideoPlaylist.fldPlaylistID = tblPlaylist.fldPlaylistID;");
+        do{
+            String data = DB.getDisplayData();
+            if (data.equals(DB.NOMOREDATA)){
+                break;
+            }else{
+                imagePath.add(data);
+
+            }
+        } while(true);
+
+        for (int i = 0; i <imagePath.size() ; i++) {
+            //System.out.println(imagePath.get(i));
+            String path = imagePath.get(i);
+            String videoP = Controller.videoPath.get(i);
+            //System.out.println(videoPath.get(i));
+            ImageView thumbnail = new ImageView();
+            thumbnail.setFitWidth(201);
+            thumbnail.setFitHeight(250);
+            thumbnail.setPreserveRatio(true);
+            thumbnail.setImage(new Image(path));
+            //mediaPlaylistGrid.addRow(i + 1);
+            mediaPlaylistGrid.addColumn(i+1, thumbnail);
+            GridPane.setHalignment(thumbnail, HPos.CENTER);
+            mediaPlaylistGrid.setHgap(40);
+            thumbnail.setOnMouseClicked(event -> handleThumbnailPlay(videoP));
+        }
+
+        buttonFullscreen.setOnMouseClicked(event -> handleFullscreenMax() );
+
+
+
+
         System.out.println(Controller.videoPath.get(0));
 
         String path1 = mediaPath;
@@ -63,7 +178,6 @@ public class MediaPlayerController implements Initializable {
         System.out.println(path1);
         // Create new Media object (the actual media content)
         String tester = "";
-        tester = tester + Controller.videoPath.get(0);
 
 
         me = new Media(new File(path1).toURI().toString());
@@ -83,6 +197,21 @@ public class MediaPlayerController implements Initializable {
             }
         });
 
+
+        sliderSeek.setMin(1);
+
+        sliderSeek.setBlockIncrement(1.0);
+        sliderSeek.valueProperty().addListener(sliderChangeListener);
+
+        mp.currentTimeProperty().addListener(currentTimeChangeListener);
+
+        sliderSound.valueProperty().addListener(sliderSoundListener);
+
+
+
+
+
+
     }
 
     @FXML
@@ -92,21 +221,112 @@ public class MediaPlayerController implements Initializable {
     private void handlePlay()
     {
         mp.play();
+        labelTotalDuration.setText(String.valueOf((int)mp.getTotalDuration().toSeconds()));
+        sliderSound.setValue(1.0);
+        imagePlay.setImage(new Image("presentation/ressources/icons/pause.png"));
+        play.setOnAction(event -> handlePause());
+
+
+
     }
 
     @FXML private void handleStop()
     {
         mp.stop();
+        sliderSeek.setValue(0);
+        imagePlay.setImage(new Image("presentation/ressources/icons/play.png"));
+        play.setOnAction(event -> handlePlay());
     }
 
     @FXML private void handlePause()
     {
         mp.pause();
+        imagePlay.setImage(new Image("presentation/ressources/icons/play.png"));
+        play.setOnAction(event -> handlePlay());
     }
 
     @FXML private void handleMute()
     {
         mp.setMute(true);
+        mute.setOnAction(event -> handleUnmute());
+        sliderSound.setValue(0);
+    }
+
+
+    @FXML private void handleUnmute()
+    {
+        mp.setMute(false);
+        sliderSound.setValue(100);
+        mute.setOnAction(event -> handleMute());
+    }
+
+    @FXML private void handleThumbnailPlay(String videoPath) {
+
+        mp.stop();
+        imagePlay.setImage(new Image("presentation/ressources/icons/play.png"));
+
+        String path1 = videoPath;
+        path1 = path1.replaceAll("\\s+","");
+        System.out.println(path1);
+        // Create new Media object (the actual media content)
+        me = new Media(new File(path1).toURI().toString());
+        // Create new MediaPlayer and attach the media to be played
+        mp = new MediaPlayer(me);
+        //
+        mediaV.setMediaPlayer(mp);
+        // If autoplay is turned of the method play(), stop(), pause() etc controls how/when medias are played
+        //mp.setAutoPlay(false);
+
+        mp.currentTimeProperty().addListener(currentTimeChangeListener);
+
+
+
+    }
+
+    @FXML private void handleNext() {
+
+        mp.stop();
+        imagePlay.setImage(new Image("presentation/ressources/icons/play.png"));
+
+        String path1 = it.next();
+        path1 = path1.replaceAll("\\s+","");
+        System.out.println(path1);
+        // Create new Media object (the actual media content)
+        me = new Media(new File(path1).toURI().toString());
+        // Create new MediaPlayer and attach the media to be played
+        mp = new MediaPlayer(me);
+        //
+        mediaV.setMediaPlayer(mp);
+        // If autoplay is turned of the method play(), stop(), pause() etc controls how/when medias are played
+        //mp.setAutoPlay(false);
+
+        mp.currentTimeProperty().addListener(currentTimeChangeListener);
+
+
+
+    }
+
+    @FXML private void handlePrevious() {
+
+        mp.stop();
+        imagePlay.setImage(new Image("presentation/ressources/icons/play.png"));
+
+        String path1 = it.previous();
+        path1 = path1.replaceAll("\\s+","");
+        System.out.println(path1);
+        // Create new Media object (the actual media content)
+        me = new Media(new File(path1).toURI().toString());
+        // Create new MediaPlayer and attach the media to be played
+        mp = new MediaPlayer(me);
+        //
+        mediaV.setMediaPlayer(mp);
+        // If autoplay is turned of the method play(), stop(), pause() etc controls how/when medias are played
+        //mp.setAutoPlay(false);
+
+        mp.currentTimeProperty().addListener(currentTimeChangeListener);
+
+
+
     }
 
     @FXML private void handleBack(Stage stage1) throws IOException {
@@ -114,6 +334,32 @@ public class MediaPlayerController implements Initializable {
         stage1.setTitle("Video Player");
         stage1.setScene(new Scene(root, 800, 1000));
         mp.stop();
+
+    }
+
+    @FXML private void handleSliderSeek() {
+        System.out.println((int)sliderSeek.getValue());
+        mp.seek(Duration.seconds((int)sliderSeek.getValue()));
+    }
+
+
+    @FXML private void handleFullscreenMax() {
+        mediaPlaylistGrid.setVisible(false);
+        DoubleProperty bindWidth = mediaV.fitWidthProperty();
+        DoubleProperty bindHeight = mediaV.fitHeightProperty();
+        bindWidth.bind(Bindings.selectDouble(mediaV.sceneProperty(), "width"));
+        bindHeight.bind(Bindings.selectDouble(mediaV.sceneProperty(), "height"));
+        buttonFullscreen.setOnMouseClicked(event -> handleFullscreenMin());
+
+    }
+
+    @FXML private void handleFullscreenMin() {
+        mediaV.fitWidthProperty().unbind();
+        mediaV.fitHeightProperty().unbind();
+        mediaV.fitHeightProperty().setValue(900);
+        mediaV.fitWidthProperty().setValue(900);
+        buttonFullscreen.setOnMouseClicked(event -> handleFullscreenMax() );
+        mediaPlaylistGrid.setVisible(true);
 
     }
 
